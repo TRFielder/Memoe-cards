0cac50a7a225a1e3ce4332d5a10d10e578d81b45
Tom Fielder <tom.fielder97@gmail.com>
2022-03-01 22:55:50 +0000

    Begun creating game component and have added getCharacterData() to this component, as this will now tell Cards.js what character to put on each card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Cards from './components/cards';
+import Game from './components/game';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Cards num="1" name="ayaka" vision="cryo"/>
-        <Cards num="2" name="sara" vision="electro"/>
-        <Cards num="3" name="raiden" vision="electro"/>
+        <Game />
       </header>
     </div>
   );
