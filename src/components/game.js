caba871bd3dc72a1b4ba10af81d5a3a02327eb5c
Tom Fielder <tom.fielder97@gmail.com>
2022-03-04 22:07:11 +0000

    Now generating cards as required in game.js - need to ensure no duplicates

---
+++ src/components/game.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Cards from "./cards";
+import "./game.css";
+
+const Game = () => {
+
+    const [level, setLevel] = useState(1)
+    const [characters, setCharacters] = useState([])
+
+    async function getCharacterData() {
+        let response = await fetch("https://api.genshin.dev/characters/")
+        let characterList = await response.json()
+        //Remove Thoma from character list as API does not have icon-big for this character (sorry Thoma!)
+        const noIconCharacterList = characterList.filter(function(char) {
+            return char !== "thoma" && char !== "traveler-anemo" && char !== "traveler-electro" && char !== "traveler-geo" 
+        });
+
+        let charName = (noIconCharacterList[Math.floor(Math.random()*noIconCharacterList.length)])
+
+        let visionResponse = await fetch (`https://api.genshin.dev/characters/${charName}`)
+        let characterData = await visionResponse.json();
+
+        let charObj = {
+            name: charName,
+            vision: characterData.vision
+        }
+
+        return charObj
+    }
+
+    const decrementLevel = () => {
+        setLevel(level-1)
+    }
+
+    const incrementLevel = () => {
+        setLevel(level+1)
+    }
+
+    const newCharacters = () => {
+        setCharacters([]);
+        let numCards = level+3;
+        for (let i = 0; i < numCards; i++) {
+            getCharacterData().then(result => {
+                    setCharacters(characters => [...characters, result])
+            })
+        }
+    }
+
+    return(
+        <div className="game" id="game">
+
+            <ul className="card-display">                
+                {characters.map(char => <Cards key={char.name} name={char.name} vision={char.vision} />)}               
+            </ul>
+            <button onClick={newCharacters}>NEW SET</button>
+            <button onClick={incrementLevel}>LEVEL UP</button>
+            <button onClick={decrementLevel}>LEVEL DOWN</button>
+
+        </div>
+    )
+}
+export default Game;
