edec8e34bc67299fe064f84a33335e0b4d2d7f08
Tom Fielder <tom.fielder97@gmail.com>
2022-03-05 12:40:43 +0000

    Add footer

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa"
+import "./footer.css"
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div id="footer">
+                <div className="innerFooter">
+                    <div className="logoContainer">
+                        <p>Tom Fielder 2022 <a href="https://github.com/TRFielder" target="_blank" rel="noreferrer"> <FaGithub /> </a> </p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer;
